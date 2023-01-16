59d6132e7294c372f7993edb02069f62fe81998d
Suman <parbatkhanal73@gmail.com>
2023-01-16 21:27:52 +0545

    Concatination

---
+++ hey.cs
@@ -0,0 +1,9 @@
+using System;
+public class Program{
+    public static void main(){
+        string s1="Suman";
+        string s2="Khanal";
+        Console.WriteLine(String.Concat(s1,s2));
+        Console.WriteLine(s1.Length);
+    }
+}
