1eb9fa57688732a323c13d439e2bbc7a2bb08382
Suman <parbatkhanal73@gmail.com>
2023-01-18 16:40:51 +0545

    even or odd

---
+++ EVEN.cs
@@ -0,0 +1,19 @@
+// Program to Check the given number is ODD or EVEN
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       int num;
+       Console.Write("Enter any number to check odd or even:");
+       num=int.Parse(Console.ReadLine());
+       if(num%2==0){
+           Console.Write("Even Number");
+       }
+       else{
+           Console.Write("Odd number");
+       }
+    }
+}
