bb7626388a4df651af3ed50666cc53a8560cb679
Suman <parbatkhanal73@gmail.com>
2023-01-25 19:36:46 +0545

    ArraySort

--- sort.cs
+++ sort.cs
@@ -1,7 +1,7 @@
 using System;
 class HelloWorld {
   static void Main() {
-    char[] arr={'a','m','e','z','d',"b"};
+    char[] arr={'a','m','e','z','d','b'};
     Array.Sort(arr);
     foreach(var item in arr){
         Console.WriteLine(item);
