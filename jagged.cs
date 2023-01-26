a86862c101433b9c9559f51ea78ab9e9cb47fcfe
Suman <parbatkhanal73@gmail.com>
2023-01-26 12:30:38 +0545

    Example of jagged Array

--- jagged.cs
+++ jagged.cs
@@ -4,10 +4,10 @@ public static void Main(){
     string[][] str=new string[6][];
     str[0]=new string[1] {"*"};
     str[1]=new string[2]{"*","*"};
-    str[2]=new string [4]{"*","*","*"};
+    str[2]=new string [4]{"*","*","*","*"};
     str[3]=new string [1]{"*"};
     str[4]=new string [2]{"*","*"};
-    str[5]=new string [4]{"*","*","*"};
+    str[5]=new string [4]{"*","*","*","*"};
     foreach( string[] row in str){
         foreach(string column in row){
             Console.Write(column);
