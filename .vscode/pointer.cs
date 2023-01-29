645b02b1d04bebaddb56ffb1559b1e3355092e6d
Suman <parbatkhanal73@gmail.com>
2023-01-29 19:48:58 +0545

    new exam related program

---
+++ .vscode/pointer.cs
@@ -0,0 +1,12 @@
+using System;
+public class Point{
+    public static void Main(){
+        unsafe{
+            int a=5;
+            int*b=&a;
+            Console.WriteLine("Value : "+*b);
+            Console.WriteLine("Address : "+(int)b);
+            
+        }
+    }
+}
