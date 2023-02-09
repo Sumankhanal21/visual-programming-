5662ae05aaa4acb3be35636f1c7383bb00b28409
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-09 20:27:53 +0545

    deleted

--- hey.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class Program{
-    public static void Main(){
-        string s1="Suman";
-        string s2="Khanal";
-        Console.WriteLine(String.Concat(s1,s2));
-        Console.WriteLine(s1.Length);
-    }
-}
