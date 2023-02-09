4ab42912ed8d76ec219772e7ec5b4215be782a55
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-09 21:02:35 +0545

    KeyMapping

---
+++ .vscode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Name
+{
+    
+
+public class Program
+{
+    unsafe static void  Main(){ 
+    int [] arr={6,57};
+    fixed(int*ptr=arr){
+        for(int i=0;i<2;i++){
+           Console.WriteLine("Before Mapping: "+*(ptr+i));
+          int b=*(ptr+i)+1;
+            Console.WriteLine("After Mapping: "+b);
+        }
+
+    }
+ }   
+}
+}
