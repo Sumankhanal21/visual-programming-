c65b9de4c4b8e589762bff0af3f13eb098846479
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-13 13:51:08 +0545

    Multilevel Inheritance in C#

--- .vscode/Program.cs
+++ .vscode/Program.cs
@@ -1,20 +1,41 @@
-﻿using System;
-namespace Name
-{
-    
-
-public class Program
-{
-    unsafe static void  Main(){ 
-    int [] arr={6,57};
-    fixed(int*ptr=arr){
-        for(int i=0;i<2;i++){
-           Console.WriteLine("Before Mapping: "+*(ptr+i));
-          int b=*(ptr+i)+1;
-            Console.WriteLine("After Mapping: "+b);
+﻿
+using System;
+namespace Name{
+    public class Parent{
+        public string name;
+        public string address;
+        public string aim;
+        public void Met(){
+            Console.WriteLine("Name: "+name);
+            Console.WriteLine("address: "+address);
+            Console.WriteLine("aim: "+aim);
+        }
+    }
+    public class Child2:Parent
+    {
+        public int class_;
+         public void Method(){
+            Console.WriteLine("Class: "+class_);
+         }
+    }
+    public class Child:Child2
+     {
+       public double gpa;
+        public void Metho(){
+            Console.WriteLine("GPA: "+gpa);
+        }
+        public static void Main(){
+            Child obj=new Child();
+            obj.name="SumanKhanal";
+            obj.address="Pokhara";
+            obj.aim="Computer Engineering";
+            obj.gpa=3.72D;
+            obj.class_=12;
+            obj.Met();
+            obj.Method();
+            obj.Metho();
         }
 
+
     }
- }   
-}
 }
