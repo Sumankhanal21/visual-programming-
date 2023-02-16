2d2e98b00720b95232e57fa45cacfb9e703fd31b
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-16 10:13:14 +0545

    List in C#

---
+++ .vscode/list.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program{
+    public static void Main(string[]args){
+        List<string> list=new List<string> ();
+        list.Add("Sagar sari chokho maya ");
+        list.Add("Timilai Diula");
+        list.Add("Yo jyana le satha diya ");
+        list.Add("Timi sangai jiula ");
+        for(int i=0;i<list.Count;i++){
+            Console.WriteLine(list[i]);
+        }
+
+    }
+}
