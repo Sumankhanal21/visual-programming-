f6d0d641efc665520f0e4c5359dcaac0b675c2a9
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-25 11:47:03 +0545

    Enumeration in C#

---
+++ enum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Name{
+    enum Days{
+        sunday,//0
+        monday,//1
+        tuesday=4,//4
+        wednesday,//5
+        thrusday,//6
+        friday,//7
+        saturday//8
+
+    }
+    class Enum
+    {
+        public static void Main(string[]args){
+            Days d=Days.wednesday;
+            Console.Write(d+"\n");
+            Console.WriteLine(Days.tuesday);
+            Days a =(Days)1;
+            Console.WriteLine(a);
+            Days n=(Days)8;
+            Console.WriteLine(n);
+            Console.BackgroundColor=ConsoleColor.Yellow;
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("My name is suman khanal");
+            Console.ReadLine();
+        }
+    }
+}
