c0746f0e6d1c42bb2f9e10d23f00ae2196d65817
Suman Khanal <110924301+Sumankhanal21@users.noreply.github.com>
2023-02-25 11:51:44 +0545

    Multilevel inheritance in C#

--- .vscode/Program.cs
+++ .vscode/Program.cs
@@ -1,18 +1,41 @@
-﻿using System;
-enum Color
-{
-    red,
-    blue,
-    green,
-    yellow,
-    gray,
-}
-public class Program
-{
-    public static void Main(){
-        Color p=Color.red;
-        Console.WriteLine("My favourite colour is "+p);
+﻿
+using System;
+namespace Name{
+    public class Parent{
+        public string name;
+        public string address;
+        public string aim;
+        public void Met(){
+            Console.WriteLine("Name: "+name);
+            Console.WriteLine("address: "+address);
+            Console.WriteLine("aim: "+aim);
+        }
+    }
+    public class Child2:Parent
+    {
+        public int class_;
+         public void Method(){
+            Console.WriteLine("Class: "+class_);
+         }
+    }
+    public class Child:Child2
+     {
+       public double gpa;
+        public void Metho(){
+            Console.WriteLine("GPA: "+gpa);
+        }
+        public static void Main(){
+            Child obj=new Child();
+            obj.name="SumanKhanal";
+            obj.address="Pokhara";
+            obj.aim="Computer Engineering";
+            obj.gpa=3.72D;
+            obj.class_=12;
+            obj.Met();
+            obj.Method();
+            obj.Metho();
+        }
+
 
     }
-    
 }
